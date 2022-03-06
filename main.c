fef5a89238f799fb22af010690b6ed9023d0fcb4
merve yanmaz <97607154+merveyanmz@users.noreply.github.com>
2022-03-06 15:16:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  insertion
+//
+//  Created by Merve Yanmaz on 15.12.2020.
+//
+
+#include <stdio.h>
+#define SIZE 100
+
+int main() {
+   
+       int i,k,n,ekle;
+       int ar[SIZE];
+       
+       printf("Please enter number of elements:");
+       scanf("%d",&n);
+       for (i=0; i<n; i++) {
+           printf("Please enter element %d:",i);
+           scanf("%d",&ar[i]);
+       }
+       
+       for (i=1; i<n; i++) {
+          ekle=ar[i];
+          for (k=i-1; k>=0 && ekle<=ar[k]; k--) {
+             ar[k+1]=ar[k];
+          }
+          ar[k+1]=ekle;
+       }
+    
+       for (i=0; i<n; i++) {
+          printf("%d\t",ar[i]);
+       }
+       
+    
+    return 0;
+}
